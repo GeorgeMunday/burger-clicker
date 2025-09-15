57679e6ab14f79a8635caedd1bdec010d7041afc
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2025-09-15 21:23:03 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1> this is a template for Vite + Typescript Tailwind css</h1>
+      <p className="color bg-red-600">if this is red tailwind is working</p>
+    </>
+  );
+}
+
+export default App;
