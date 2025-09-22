1350ab24313ab525e77ae4dbf8664466e4d2cff8
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2025-09-22 22:28:04 +0100

    forgott 1 detail

--- src/pages/Level1.tsx
+++ src/pages/Level1.tsx
@@ -20,10 +20,57 @@ const Level1 = ({
   onLogout,
 }: EarnPointsProps & { onLogout: () => void }) => {
   return (
-    <>
-      <div className="flex w-full justify-between items-center mb-2">
-        <Logout onLogout={onLogout} />
-        <OpenMenu
+    <main className="flex-1 flex flex-col items-center justify-center min-h-screen bg-[#f5ecd7] text-[#7c5c36] p-4">
+      <div className="w-[90vw] h-[90vh] max-w-6xl max-h-[900px] rounded-3xl bg-[#e7dbc3] shadow-2xl p-6 sm:p-8 md:p-10 flex flex-col border border-[#7c5c36] overflow-auto">
+        <div className="flex w-full justify-between items-center mb-2">
+          <Logout onLogout={onLogout} />
+          <OpenMenu
+            pointsPS={pointsPS}
+            setPoints={setPoints}
+            setPointsPS={setPointsPS}
+            points={points}
+            num={num}
+            setNum={setNum}
+            levelCap={levelCap}
+            onNextLevel={onNextLevel}
+            canGoNext={canGoNext}
+          />
+        </div>
+        <NewLevelCountDown
+          levelCap={levelCap}
+          points={points}
+          num={num}
+          setNum={setNum}
+          setPoints={setPoints}
+          pointsPS={pointsPS}
+          setPointsPS={setPointsPS}
+        />
+        <Points
+          points={points}
+          num={num}
+          setNum={setNum}
+          setPoints={setPoints}
+          pointsPS={pointsPS}
+          setPointsPS={setPointsPS}
+          levelCap={levelCap}
+          onNextLevel={onNextLevel}
+          canGoNext={canGoNext}
+        />
+        <EarnPoints
+          num={num}
+          points={points}
+          setPoints={setPoints}
+          setNum={setNum}
+          pointsPS={pointsPS}
+          setPointsPS={setPointsPS}
+          levelCap={levelCap}
+          onNextLevel={onNextLevel}
+          canGoNext={canGoNext}
+        />
+        <div className="flex-1 flex flex-col justify-end">
+          <NextLevels canGoNext={!!canGoNext} onNextLevel={onNextLevel!} />
+        </div>
+        <PointsPerSecond
           pointsPS={pointsPS}
           setPoints={setPoints}
           setPointsPS={setPointsPS}
@@ -35,52 +82,7 @@ const Level1 = ({
           canGoNext={canGoNext}
         />
       </div>
-      <NewLevelCountDown
-        levelCap={levelCap}
-        points={points}
-        num={num}
-        setNum={setNum}
-        setPoints={setPoints}
-        pointsPS={pointsPS}
-        setPointsPS={setPointsPS}
-      />
-      <Points
-        points={points}
-        num={num}
-        setNum={setNum}
-        setPoints={setPoints}
-        pointsPS={pointsPS}
-        setPointsPS={setPointsPS}
-        levelCap={levelCap}
-        onNextLevel={onNextLevel}
-        canGoNext={canGoNext}
-      />
-      <EarnPoints
-        num={num}
-        points={points}
-        setPoints={setPoints}
-        setNum={setNum}
-        pointsPS={pointsPS}
-        setPointsPS={setPointsPS}
-        levelCap={levelCap}
-        onNextLevel={onNextLevel}
-        canGoNext={canGoNext}
-      />
-      <div className="flex-1 flex flex-col justify-end">
-        <NextLevels canGoNext={!!canGoNext} onNextLevel={onNextLevel!} />
-      </div>
-      <PointsPerSecond
-        pointsPS={pointsPS}
-        setPoints={setPoints}
-        setPointsPS={setPointsPS}
-        points={points}
-        num={num}
-        setNum={setNum}
-        levelCap={levelCap}
-        onNextLevel={onNextLevel}
-        canGoNext={canGoNext}
-      />
-    </>
+    </main>
   );
 };
 
